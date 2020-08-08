c763c4a5a41d6b6e08be53da5f803da3dcccceb4
undefinedcodezhong <tjlittlezhong@163.com>
2020-08-08 22:11:11 +0800

    first commit, push a code template

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd build
+cmake ..
+make
+cd ..
+echo "Start..."
+echo "~~~~~~~~~~~~~~~~"
+./test
---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+// time: 2020-08-08
+// *题目来源：
+// ?题目描述： 
+// !完成情况：
+ 
+using namespace std;
+
+int solution()
+{
+    return 0;
+}
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    
+    int n,m;
+    cin >> n >> m;
+    cout << n << "," << m << endl;
+
+    return 0;
+}
