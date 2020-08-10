72409f9b9359966115501a48841366b37c42e7b2
undefinedcodezhong <tjlittlezhong@163.com>
2020-08-10 21:16:16 +0800

    modify build.sh,推荐使用zsh来运行

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
+set -e
+export TIMEFMT=$'\e[1;34m ~~~~~~~~~~~~~~~~~ \e[0m\n\e[1;34m    ...Finish...    \e[0m\n\e[1;34m ~~~~~~~~~~~~~~~~~ \e[0m\nthe program [\e[1;34m%J\e[0m] consumes: \nuser\t%U\nsys\t%S'
 cd build
 cmake ..
 make
 cd ..
-echo "Start..."
-echo "~~~~~~~~~~~~~~~~"
-./test
+echo "\e[1;34m ~~~~~~~~~~~~~~~~~ \e[0m"
+echo "\e[1;34m    ...Start...    \e[0m"
+echo "\e[1;34m ~~~~~~~~~~~~~~~~~ \e[0m"
+time ./test
