2ce1c890ebf764fbb119feb8631b61320ef1c919
undefinedcodezhong <tjlittlezhong@163.com>
2020-08-13 17:21:25 +0800

    modify the build script and CMakeLists.txt, reduce some cmake warning

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -e
-export TIMEFMT=$'\e[1;34m ~~~~~~~~~~~~~~~~~ \e[0m\n\e[1;34m    ...Finish...    \e[0m\n\e[1;34m ~~~~~~~~~~~~~~~~~ \e[0m\nthe program [\e[1;34m%J\e[0m] consumes: \nuser\t%U\nsys\t%S'
+export TIMEFMT=$'\n\e[1;34m ~~~~~~~~~~~~~~~~~ \e[0m\n\e[1;34m    ...Finish...    \e[0m\n\e[1;34m ~~~~~~~~~~~~~~~~~ \e[0m\nthe program [\e[1;34m%J\e[0m] consumes: \nuser\t%U\nsys\t%S'
 cd build
 cmake ..
 make
@@ -7,4 +7,4 @@ cd ..
 echo "\e[1;34m ~~~~~~~~~~~~~~~~~ \e[0m"
 echo "\e[1;34m    ...Start...    \e[0m"
 echo "\e[1;34m ~~~~~~~~~~~~~~~~~ \e[0m"
-time ./test
+time ./main
